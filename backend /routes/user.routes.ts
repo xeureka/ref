3ed7b1811e6468d907147a5aa9d1b14753ad0596
xeureka <156827630+xeureka@users.noreply.github.com>
2026-02-02 09:04:23 +0300

    uncomment and make the auth thigns visible

--- backend /routes/user.routes.ts
+++ backend /routes/user.routes.ts
@@ -10,73 +10,73 @@ const router = express.Router()
 
 router.get('/', seeAllUsers)
 
-// router.post('/register',validateData(registerUserSchema), async (req,res) => {
-//     try {
+router.post('/register',validateData(registerUserSchema), async (req,res) => {
+    try {
 
-//         const existingUser = await Users.findOne({email: req.body.email}); 
+        const existingUser = await Users.findOne({email: req.body.email}); 
 
-//         if (existingUser){
-//             res.status(404).json({message: "User Already Registered !"})
-//             return;
-//         }
+        if (existingUser){
+            res.status(404).json({message: "User Already Registered !"})
+            return;
+        }
 
-//         let securePassword = await hashPassword(req.body.password);
+        let securePassword = await hashPassword(req.body.password);
 
-//         let newUser = new Users({
-//             name: req.body.name,
-//             email: req.body.email,
-//             password: securePassword
-//         })
+        let newUser = new Users({
+            name: req.body.name,
+            email: req.body.email,
+            password: securePassword
+        })
 
-//         const savedNewUser = await newUser.save()
+        const savedNewUser = await newUser.save()
 
-//         const token = generateToken(newUser.email, newUser.role)
-//         res.header('authoziation',token)
-//         res.status(201).json({message: 'User Registered Successfully !!'})
+        const token = generateToken(newUser.email, newUser.role)
+        res.header('authoziation',token)
+        res.status(201).json({message: 'User Registered Successfully !!'})
 
-//     } catch (error) {
-//         console.log('Error Registering the user!')
-//         res.status(400).json({message: error})
-//     }
-// })
+    } catch (error) {
+        console.log('Error Registering the user!')
+        res.status(400).json({message: error})
+    }
+})
 
-// router.post('/login',validateData(loginUserSchema), async (req,res) => {
-//     try {
-//         const isUserExist = await Users.findOne({email: req.body.email});
-//         if (!isUserExist){
-//             res.status(404).json({message: 'User Does not exist please register !'})
-//             return;
-//         }
+router.post('/login',validateData(loginUserSchema), async (req,res) => {
+    try {
+        const isUserExist = await Users.findOne({email: req.body.email});
+        if (!isUserExist){
+            res.status(404).json({message: 'User Does not exist please register !'})
+            return;
+        }
 
-//         const { password } = req.body;
-//         const validPassword = validatePassword(password,isUserExist.password);
+        const { password } = req.body;
+        const validPassword = validatePassword(password,isUserExist.password);
 
-//         if (!validPassword){
-//             res.status(401).json({message: "Email or Password Incorrect !!"})
-//             return;
-//         }
+        if (!validPassword){
+            res.status(401).json({message: "Email or Password Incorrect !!"})
+            return;
+        }
 
-//         const token = generateToken(isUserExist.email, isUserExist.role)
-//         res.header('authorization',token)
-//         res.status(201).json('Login Successful !!')
+        const token = generateToken(isUserExist.email, isUserExist.role)
+        res.header('authorization',token)
+        res.status(201).json('Login Successful !!')
 
-//     } catch (error) {
-//         res.status(400).json({message: 'Error loggin the user!'});
-//         console.log(error)
-//         return;
-//     }
-// })
+    } catch (error) {
+        res.status(400).json({message: 'Error loggin the user!'});
+        console.log(error)
+        return;
+    }
+})
 
-// // path to logout the user
-// router.post('/logout', async (req,res) => {
-//     try {
-//         res.clearCookie('Authorization').json({
-//             message: "Logged Out Successfully !!"
-//         }).status(200)
-//     } catch (error) {
-//         console.log('Error Logging Out the User !!')
-//     }
-// })
+// path to logout the user
+router.post('/logout', async (req,res) => {
+    try {
+        res.clearCookie('Authorization').json({
+            message: "Logged Out Successfully !!"
+        }).status(200)
+    } catch (error) {
+        console.log('Error Logging Out the User !!')
+    }
+})
 
 export default router
 
